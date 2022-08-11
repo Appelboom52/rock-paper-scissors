71842770fbff2817d601a291c0e3a65c8834a7c0
Andrej <andrzejapfelbaum@gmail.com>
2022-08-11 17:07:11 +0300

    finish a game of rock-paper-scissors

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+I created a simple Rock-Paper-Scissors game. The game asks you for your choice, then compares it with computer's random selection, shows you the result in the console, and repeats aforementioned actions four more times. 
--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-let randomNumber = 0;
-let computerChoice;
+
+const computerSelection = getComputerChoice();
+let playerSelection;
+let result;
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
-    randomNumber = Math.floor(Math.random() * 3 + 1);
-    if (randomNumber === 1) {
-        computerChoice = 'rock';
-    } else if 
-        (randomNumber === 2) {
-            computerChoice = 'paper';
-        }
-    else computerChoice = 'scissors';
-    return computerChoice;
+    const array = ['rock', 'paper', 'scissors'];
+    return array[Math.floor(Math.random() * 3)]; 
     }
 
+    
+function playRound() {
+    if ((playerSelection === 'rock' && computerSelection === 'paper') 
+      || (playerSelection === 'paper' && computerSelection === 'scissors') 
+      || (playerSelection === 'scissors' && computerSelection === 'rock'))
+       {
+        return result = `${computerSelection} beats ${playerSelection}, current score is ${playerScore}:${++computerScore}`;
+    }
+       else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
+       || (playerSelection === 'paper' && computerSelection === 'rock') 
+       || (playerSelection === 'scissors' && computerSelection === 'paper'))
+       {
+        return result = `${playerSelection} beats ${computerSelection}, current score is ${++playerScore}:${computerScore}`;  
+    }
+    else { 
+        return result = `It is a tie, current score is ${playerScore}:${computerScore}`;
+    }
+    console.log(result);
+    console.log(`you ${playerScore}:${computerScore} computer`);
+}
 
-getComputerChoice();
-console.log(computerChoice);
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        checkForValidity();
+        console.log(`computer: ${computerSelection}`);
+        console.log(`you: ${playerSelection}`);
+    }
+    console.log(`Final score is ${playerScore}:${computerScore}`);
+}
+
+
+function checkForValidity() {
+    playerSelection = prompt('enter "rock", "paper", or "scissors":').toLowerCase();
+    if ((playerSelection.toLowerCase() === 'rock')
+        || (playerSelection.toLowerCase() === 'paper')
+        || (playerSelection.toLowerCase() === 'scissors')) {
+            console.log(playRound(playerSelection, computerSelection))
+        }
+        else {
+            console.log("enter 'rock', 'paper', or 'scissors', you cheater");
+            checkForValidity();
+        }
+}
+game();
+
